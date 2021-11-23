fcb8d8cb355bcd8948d472a5225d212a7c57ccb5
RomanOliva10 <85895114+RomanOliva10@users.noreply.github.com>
2021-11-23 20:59:52 -0300

    [MAKE/DIR/FILE] Actividad 1

---
+++ Semana 5/Dia 2/Actividad 1/main.js
@@ -0,0 +1,8 @@
+const navlinks = document.getElementById("nav-links");
+function showLikns() {
+    if (navlinks.style.display === "block") {
+      navlinks.style.display = "none";
+    } else {
+      navlinks.style.display = "block";
+    }
+  }
