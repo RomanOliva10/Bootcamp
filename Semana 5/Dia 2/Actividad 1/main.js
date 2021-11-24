270d23e3bb0d6bbf3cb636c12e51f3686549288c
RomanOliva10 <85895114+RomanOliva10@users.noreply.github.com>
2021-11-23 21:08:48 -0300

    [MAKE/DIR/FILE] Actividad 1

--- Semana 5/Dia 2/Actividad 1/main.js
+++ Semana 5/Dia 2/Actividad 1/main.js
@@ -1,8 +1,11 @@
 const navlinks = document.getElementById("nav-links");
+const hamburger = document.getElementById("hamburger");
 function showLikns() {
     if (navlinks.style.display === "block") {
       navlinks.style.display = "none";
+      hamburger.className = 'fas fa-bars';
     } else {
+      hamburger.className = 'fas fa-times';
       navlinks.style.display = "block";
     }
   }
